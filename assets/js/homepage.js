99b3ad2aff2bcf83c10118d16aa2d58ba11d2549
Wilson <wilsonhorrell@git.training>
2020-07-06 18:44:42 -0500

    Progressing nicely

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -13,15 +13,15 @@ var formSubmitHandler = function(event) {
 userFormEl.addEventListener("submit", formSubmitHandler);
 
 var displayRepos = function(repos, searchTerm) {
-    console.log(repos);
-    console.log(searchTerm);
+
     repoContainerEl.textContent = '';
     repoSearchTerm.textContent = searchTerm;
     for (var i = 0; i < repos.length; i++) {
         var repoName = repos[i].owner.login + '/' + repos[i].name;
 
-        var repoEl = document.createElement("div");
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html");
 
         var titleEl = document.createElement("span");
         titleEl.textContent = repoName;
